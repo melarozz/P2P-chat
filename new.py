4e2e128bd7a834fa4c17f0bae0596fac963dec17
Valeria <64836918+melarozz@users.noreply.github.com>
2024-05-18 16:02:12 +0700

    Update new.py

--- new.py
+++ new.py
@@ -8,7 +8,7 @@ def read_socket_and_output(s):
     while True:
         if bye_flag:
             try:
-                received_str = s.recv(100).decode()
+                received_str = s.recv(1024).decode()
                 print("\r>>> " + received_str + "\n<<<", end="", flush=True)
             except:
                 print("Connection closed")
@@ -27,16 +27,26 @@ def read_stdin_and_write_socket(s):
     while True:
         if bye_flag:
             send_str = input("<<< ")
-            s.send(send_str.encode())
+            s.sendto(send_str.encode(), (broadcast_ip, port))
             if send_str == "bye":
                 print("Chat termination signal sent!!!")
                 bye_flag = 0
                 s.close()
                 connect_to_peer()
 
+def broadcast_message():
+    global bye_flag
+    while True:
+        if bye_flag:
+            broadcast_str = input("Broadcast Message: ")
+            s.sendto(broadcast_str.encode(), (broadcast_ip, port))
+            if broadcast_str == "bye":
+                bye_flag = 0
+                s.close()
+                connect_to_peer()
+
 def connect_to_peer():
     global bye_flag
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     bye_flag = 1
     ch = input("Connect[1] to peer or wait[2] for peer connection. Enter choice:")
 
@@ -46,19 +56,29 @@ def connect_to_peer():
         s.connect((host, port))
         threading.Thread(target=read_socket_and_output, args=(s,)).start()
         threading.Thread(target=read_stdin_and_write_socket, args=(s,)).start()
+        threading.Thread(target=broadcast_message).start()
         
     elif ch == "2":
         host = ''
         port = 54321
         s.bind((host, port))
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         s.listen(2)
         print("Waiting for connection...")
         c, addr = s.accept()  # Establish connection with client.
         threading.Thread(target=read_socket_and_output, args=(c,)).start()
         threading.Thread(target=read_stdin_and_write_socket, args=(c,)).start()
+        threading.Thread(target=broadcast_message).start()
 
     else:
         print("Incorrect choice")
         sys.exit()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+broadcast_ip = '<broadcast>'
+port = 54321
 
 connect_to_peer()
