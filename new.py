5fcdcb3cc4456e2d61b91fe1b0d9be6c66dd7730
Valeria <64836918+melarozz@users.noreply.github.com>
2024-05-18 15:51:59 +0700

    Update new.py

--- new.py
+++ new.py
@@ -2,44 +2,39 @@ import socket
 import sys
 import threading
 
-
 def readSocketAndOutput(s):
     global byeFlag
     print("Enter 'bye' to quit chat")
     while True:
         if byeFlag:
             try:
-                str = s.recv(100).decode()
-                print("\r>>> " + str + "\n<<<", end="", flush=True)
+                str_recv = s.recv(100).decode()
+                print("\r>>> " + str_recv + "\n<<<", end="", flush=True)
             except:
                 print("Connection closed")
                 break
 
-            if str == "bye":
-                byeFlag = 0;
+            if str_recv == "bye":
+                byeFlag = 0
                 break
         
     print("Remote user disconnected!!!")
     s.close()
-    sys.exit()
     
 def readSTDINandWriteSocket(s):
     global byeFlag
     while True:
         if byeFlag:
-            str = input("<<< ")
-            s.send(str.encode())
-            if str == "bye":
+            str_send = input("<<< ")
+            s.send(str_send.encode())
+            if str_send == "bye":
                 print("Chat termination signal sent!!!")
-                byeFlag = 0;
+                byeFlag = 0
                 s.close()
-                sys.exit()
-        
-
-
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-byeFlag = 1;
+byeFlag = 1
+
 ch = input("Connect[1] to peer or wait[2] for peer connection. Enter choice:")
 
 if ch == "1":
@@ -53,16 +48,12 @@ elif ch == "2":
     host = ''
     port = 54321
     s.bind((host, port))
-    s.listen(2)              # Now wait for client connection.
+    s.listen(2)
     print("Waiting for connection...")
-    while True:
-        c, addr = s.accept()     # Establish connection with client.
-        threading.Thread(target=readSocketAndOutput, args=(c,)).start()
-        threading.Thread(target=readSTDINandWriteSocket, args=(c,)).start()
+    c, addr = s.accept()  # Establish connection with client.
+    threading.Thread(target=readSocketAndOutput, args=(c,)).start()
+    threading.Thread(target=readSTDINandWriteSocket, args=(c,)).start()
 
 else:
     print("Incorrect choice")
     sys.exit()
-
-
-
