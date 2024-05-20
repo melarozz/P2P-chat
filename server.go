80431c245c028dd9f9b276c6cdae261cd65bd351
Valeria <64836918+melarozz@users.noreply.github.com>
2024-05-20 11:09:45 +0700

    Update server.go

--- server.go
+++ server.go
@@ -8,22 +8,19 @@ import (
   "strings"
 )
 
-func getEnv(key string, defaultVal string) string {
+func getEnv(key string, value string) string {
   if value, exists := os.LookupEnv(key); exists {
     return value
   }
 
-  return defaultVal
+  return value
 }
 
 func main() {
   var host = getEnv("HOST", "0.0.0.0")
 
-  go startListener(host)
+  go startListen(host)
   fmt.Printf("Listener started at port 8080 on host %v\n", host)
-  for {
-
-  }
 }
 
 func startSender(conn *net.UDPConn) {
@@ -31,6 +28,7 @@ func startSender(conn *net.UDPConn) {
     host, message := inputMessage()
     if host == "all" {
       host = "255.255.255.255"
+      message = "(for all) " + message
     }
     sendMessage(host, message, conn)
   }
@@ -38,20 +36,14 @@ func startSender(conn *net.UDPConn) {
 
 func sendMessage(addr string, message string, conn *net.UDPConn) {
   addr_, err := net.ResolveUDPAddr("udp", addr+":8080")
-  if err != nil {
-    panic(err)
-  }
   _, err = conn.WriteTo([]byte(message), addr_)
-  if err != nil {
-    fmt.Printf("Couldn't send response %v", err)
-  }
 }
 
 func inputMessage() (string, string) {
-  fmt.Printf("Message: ")
+  fmt.Printf("Enter message: ")
   reader := bufio.NewReader(os.Stdin)
   input, _ := reader.ReadString('\n')
-  var splitInput = strings.Split(input, " ")
+  var splitInput = strings.SplitN(input, " ", 2)
   host, message := splitInput[0], splitInput[1]
   return host, message
 }
@@ -59,23 +51,16 @@ func inputMessage() (string, string) {
 func listen(ser *net.UDPConn, p []byte) {
   _, remote, err := ser.ReadFromUDP(p)
   fmt.Printf("New message from %v: %s", remote, p)
-  if err != nil {
-    fmt.Printf("Some error  %v", err)
-  }
-  fmt.Printf("Message: ")
+  fmt.Printf("Enter message: ")
 }
 
-func startListener(ip string) {
+func startListen(ip string) {
   p := make([]byte, 2048)
   addr := net.UDPAddr{
     Port: 8080,
     IP:   net.ParseIP(ip),
   }
   ser, err := net.ListenUDP("udp", &addr)
-  if err != nil {
-    fmt.Printf("Some error %v\n", err)
-    return
-  }
   go startSender(ser)
   for {
     listen(ser, p)
